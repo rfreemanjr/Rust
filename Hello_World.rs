e15bdb47fa9026810e6942fc3cd88b6f5b48cdf5
rfreemanjr <rdfreejr@gmail.com>
2021-12-21 11:44:51 -0500

    Test commit of inital code

---
+++ Hello_World.rs
@@ -0,0 +1,13 @@
+/***
+ * This is a simple "Hello! World!" program
+ * as a stepping stone in learning Rust. 
+ */
+
+
+/* The main Method */
+fn main() {
+
+    /**Print statement. */
+    println!("Hello World!")
+
+}
